33df6f1bccf3e331935c1345bc6f36dc1fcd8757
iotsharing.com <nha.tuan84@gmail.com>
2018-03-12 07:35:02 +0700

    Update log_reg.py

--- log_reg.py
+++ log_reg.py
@@ -2,14 +2,46 @@ import numpy as np
 import matplotlib.pyplot as plt
 from numpy.linalg import inv
 
-x = []
-y = []
-
+init = False
 file = open('demo1x.dat', 'rb')
 for row in file:
-	r = row.strip().split(' ')
-	x.append([np.float(r[0]), np.float(r[len(r)-1])])
-
+    r = row.strip().split(' ')
+    if(init == False):
+        x_train = np.array([[1], [np.float(r[0])], [np.float(r[len(r)-1])]])
+        init = True
+    else:
+        x_train = np.append(x_train, [[1], [np.float(r[0])], [np.float(r[len(r)-1])]], axis=1);
+init = False
 file = open('demo1y.dat', 'rb')
-y = [np.float(row.strip()) for row in file]
+for row in file:
+    if(init == False):
+        y_train = np.array([[np.float(row.strip())]])
+        init = True
+    else:
+        y_train = np.append(y_train, [[np.float(row.strip())]], axis=1);
 
+m = y_train.shape[1]
+
+theta = np.array(np.zeros((x_train.shape[0], 1)))
+
+xT = x_train.T
+while True:
+    J = 0
+    #scan through training set
+    for i in range(0, m):
+        #for each training set
+        x = x + 1;
+        #calculate h_predicted
+        h = xT[i].dot(theta)
+        #calculate error=h_predicted-y_target
+        error = (h - y_train[i])
+        #accumulate error to J
+        J = J + error;
+        #update theta for a training set
+        theta = theta - 0.0001*(error*xT[i])[:, np.newaxis];
+    J=J/m
+    #plot J
+    update_line(g, x, J)
+    print(J)
+    if(abs(J)<0.0001):
+        break
