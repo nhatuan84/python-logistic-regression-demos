637edf2593b1f7d558b0aaab4aab9c333f2d5aca
iotsharing.com <nha.tuan84@gmail.com>
2018-03-19 10:57:45 +0700

    Update logistic-regression-newton-method-regularization.py

--- logistic-regression-newton-method-regularization.py
+++ logistic-regression-newton-method-regularization.py
@@ -4,7 +4,7 @@ from numpy.linalg import inv
 
 #we read data from files line by line
 init = False
-file = open('demo2x.dat', 'rb')
+file = open('ex5Logx.dat', 'rb')
 for row in file:
     r = row.decode('utf8').strip().split(',')
     if(init == False):
@@ -16,7 +16,7 @@ for row in file:
         x2 = np.append(x2, [np.float(r[1])])
 
 init = False
-file = open('demo2y.dat', 'rb')
+file = open('ex5Logy.dat', 'rb')
 for row in file:
     if(init == False):
         y_train = np.array([[np.float(row.strip())]])
@@ -47,20 +47,22 @@ neg = np.flatnonzero(y_train == 0)
 plt.plot(x_train[1, pos], x_train[2, pos], 'ro')
 plt.plot(x_train[1, neg], x_train[2, neg], 'bo') 
 
+#modify lamda to see results
 lamda = 1
 
 one = np.identity(x_train.shape[0])
 one[0,0] = 0
+
 for x in range(0, 15):
     h = sigmoid(theta.T.dot(x_train))
     error = h - y_train
     tmp = (-1)*y_train*np.log(h) - (1-y_train)*np.log((1-h))
-    J = np.sum(tmp)/m;
-    #calculate H
+    J = np.sum(tmp)/m + lamda*theta[1::,0].T.dot(theta[1::,0])/2*m
+    #calculate H with regularization from 1
     H = (h*(1-h)*(x_train)).dot(x_train.T)/m + lamda*one/m
-    #calculate dJ
+    #calculate dJ with regularization from 1
     dJ = np.sum(error*x_train, axis=1)/m
-    dJ[1:] = dJ[1:] - lamda*theta.T[0,1::]
+    dJ[1:] = dJ[1:] - lamda*theta.T[0,1::]/m
     #gradient = H-1.dJ
     grad = inv(H).dot(dJ)
     #update theta
@@ -69,18 +71,18 @@ for x in range(0, 15):
 
 print(theta)
 
+#plot boundary 
 u = np.linspace(-1, 1.5, 200)
 v = np.linspace(-1, 1.5, 200)
-
-xxx, yyy = np.meshgrid(u, v, sparse=False)
-plotz = np.zeros(xxx.shape)
-
-for i in range(0, len(xxx)):	
-	zz = theta.T.dot(create_feature(xxx[i], yyy[i]))
-	for j in range(0,zz.shape[1]):
-		plotz[i][j] = zz[0][j]
-
-cp = plt.contourf(xxx, yyy, plotz, linestyles='dashed')
-
+#create input grid
+uu, vv = np.meshgrid(u, v, sparse=False)
+#output contour
+plotz = np.zeros(uu.shape)
+#evaluate z
+for i in range(0, len(uu)):	
+	z = theta.T.dot(create_feature(uu[i], vv[i]))
+	for j in range(0,z.shape[1]):
+		plotz[i][j] = z[0][j]
+#plot contour
+plt.contourf(uu, vv, plotz, linestyles='dashed')
 plt.show()
-
